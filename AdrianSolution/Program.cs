0f08b603eb2f5593c8d5998d98faf89298b58b26
BIT8\adrian.duca <adrian.duca@bit8.com>
2019-01-17 15:42:14 +0100

    Added gitignore, and solution for question

---
+++ AdrianSolution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdrianSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Sum(long.Parse(args[0]), long.Parse(args[1]));
+        }
+
+        public static long Sum(long num1, long num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
